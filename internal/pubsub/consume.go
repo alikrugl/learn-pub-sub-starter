cdca1eaac540e24bb813e27e242e9d76172ef038
Alik Kruhlienia <alikrugl11@gmail.com>
2024-08-20 19:14:32 +0300

    declare queue

---
+++ internal/pubsub/consume.go
@@ -0,0 +1,47 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType uint8,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	isDurable := simpleQueueType == amqp.Persistent
+	isTransient := simpleQueueType == amqp.Transient
+	queue, err := ch.QueueDeclare(
+		queueName,   // name
+		isDurable,   // durable
+		isTransient, // delete when unused
+		isTransient, // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // no-wait
+		nil,        // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+
+	return ch, queue, nil
+}
--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -8,15 +8,12 @@ import (
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
-	b, err := json.Marshal(val)
+	dat, err := json.Marshal(val)
 	if err != nil {
 		return err
 	}
-
-	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
 		ContentType: "application/json",
-		Body:        b,
+		Body:        dat,
 	})
-
-	return nil
 }
